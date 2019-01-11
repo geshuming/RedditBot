387b9bc123609f1d122aa40334f4f51cedd3b0e9
Shuming <43642522+geshuming@users.noreply.github.com>
2019-01-11 19:54:25 +0800

    added fetch functions to fetch posts
    Signed-off-by: Shuming <geshuming2@gmail.com>

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from telegram.ext import Updater, InlineQueryHandler, CommandHandler
 import praw
 import prawcore
 import logging
+import re
 
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.DEBUG)
 
@@ -12,21 +13,49 @@ submissiondata = []
 
 def help(bot, update):
     message = '/all : access frontpage and fetches the hottest posts\n'\
-        '/subreddit <arg> : access the subreddit and fetches the hottest posts\n'
+        '/subreddit <subreddit> : access the subreddit and fetches the hottest posts\n'\
+        '/fetch <subreddit> : fetches a fresh unseen post from the subreddit <subreddit>\n'\
+        '/reset <subreddit> : resets already seen posts'
 
     update.message.reply_text(message)
 
-
+# deprecated
 def reset():
     subredditdata = None
     submissiondata = []
+
+
+def viewmedia(bot, chat_id, submission):
+    if submission.url.endswith(('.jpg','.png')):
+        bot.send_photo(chat_id, submission.url, caption='Title: {}'.format(submission.title))
+    else:
+        bot.send_message(chat_id, text='Title: {}\nURL: {}\n'
+                         .format(submission.title, submission.shortlink),
+                         disable_web_page_preview=True)
+
+
+def view(bot, update):
+    chatid = update.message.chat_id
+    _, query = update.message.text.split()
+    try:
+        number = int(query) - 1
+        if number < 0 or number >= len(submissiondata):
+            bot.send_message(chatid, text='Invalid post number!', disable_web_page_preview=True)
+        elif submissiondata:
+            submission = submissiondata[number]
+            viewmedia(bot, chatid, submission)
+        else:
+            bot.send_message(chatid, text='Pick a subreddit first', disable_web_page_preview=True)
+    except Exception as e:
+        print(e)
+        bot.send_message(chatid, text='Invalid post number!', disable_web_page_preview=True)
 
 
 def all(bot, update):
     chatid = update.message.chat_id
     frontpage = reddit.front.hot(limit=5)
     i = 1
-    message = "Fresh from r/all\n\n"
+    message = 'Fresh from r/all\n\n'
     for submission in frontpage:
         if i > 3:
             break
@@ -38,14 +67,16 @@ def all(bot, update):
             i = i + 1
     bot.send_message(chatid, text=message, disable_web_page_preview=True)
 
-def subreddit(bot, update):
+
+def getsubreddit(bot, update):
     _, query = update.message.text.split()
     chatid = update.message.chat_id
-    subreddit = reddit.subreddit(query)
     try:
+        subreddit = reddit.subreddit(query)
         if subreddit.over18:
             bot.send_message(chatid, text='Hey! This subreddit is NSFW!')
         else:
+            reset()
             subredditdata = subreddit.hot(limit=5)
             i = 1
             message = 'Fresh from r/{}\n\n'.format(query)
@@ -55,6 +86,7 @@ def subreddit(bot, update):
                 elif submission.stickied or submission.over_18:
                     continue
                 else:
+                    submissiondata.append(submission)
                     message += 'Title: {}\nUpvotes: {}\nLink: {}\n\n'\
                         .format(submission.title, submission.score, submission.shortlink)
                     i = i + 1
@@ -65,10 +97,64 @@ def subreddit(bot, update):
         bot.send_message(chatid, text="Subreddit {} does not exist!".format(query))
 
 
+def resetposts(bot, update):
+    _, *query = update.message.text.split()
+    if not query or query[0] == 'all':
+        srlist = reddit.front.hot(limit=10)
+        for submission in srlist:
+            if submission.hidden:
+                submission.unhide()
+        bot.send_message(update.message.chat_id, text='All done!')
+    else:
+        try:
+            srlist = reddit.subreddit(query[0]).hot(limit=10)
+            for submission in srlist:
+                if submission.hidden:
+                    submission.unhide()
+            bot.send_message(update.message.chat_id, text='All done!')
+        except prawcore.exceptions.Redirect or prawcore.exceptions.NotFound:
+            bot.send_message(update.message.chat_id, text="The subreddit /r/{} does not exist!".format(query[0]))
+
+
+def sendfreshpost(bot, update, sr):
+    srlist = sr.hot(limit=10)
+    sent = False
+    for submission in srlist:
+        if submission.stickied or submission.over_18 or submission.hidden:
+            continue
+        else:
+            submission.hide()
+            viewmedia(bot, update.message.chat_id, submission)
+            sent = True
+            break
+    if not sent:
+        bot.send_message(update.message.chat_id, text='Sorry, we seem to have ran out of fresh content')
+
+
+def fetchfresh(bot, update):
+    _, *query = update.message.text.split()
+    if not query or query[0] == 'all':
+        sendfreshpost(bot, update, reddit.front)
+    else:
+        try:
+            sr = reddit.subreddit(query[0])
+            if sr.over18:
+                bot.send_message(update.message.chat_id, text="Sorry, the subreddit /r/{} is NSFW".format(query[0]))
+            elif sr.subscribers < 1000:
+                bot.send_message(update.message.chat_id, text="The subreddit /r/{} does not exist!".format(query[0]))
+            else:
+                sendfreshpost(bot, update, sr)
+        except prawcore.exceptions.Redirect or prawcore.exceptions.NotFound:
+            bot.send_message(update.message.chat_id, text="The subreddit /r/{} does not exist!".format(query[0]))
+
+
 def main():
     updater = Updater(open('.\\TelegramTokenKey.txt').read())
     updater.dispatcher.add_handler(CommandHandler('all', all))
-    updater.dispatcher.add_handler(CommandHandler('subreddit', subreddit))
+    updater.dispatcher.add_handler(CommandHandler('subreddit', getsubreddit))
+    updater.dispatcher.add_handler(CommandHandler('fetch', fetchfresh))
+    updater.dispatcher.add_handler(CommandHandler('reset', resetposts))
+    updater.dispatcher.add_handler(CommandHandler('view', view))
     updater.dispatcher.add_handler(CommandHandler('help', help))
     updater.start_polling()
     updater.idle()
